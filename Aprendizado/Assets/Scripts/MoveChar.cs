788e8079268f06e9fba367d10f7cc455d1828a01
Baltinha <63794832+Baltinha@users.noreply.github.com>
2023-07-03 19:11:42 -0300

    Melhora na movimentação e inicio do Pulo
    3 tipos de movimentação e começo do pulo por character controller.

--- Aprendizado/Assets/Scripts/MoveChar.cs
+++ Aprendizado/Assets/Scripts/MoveChar.cs
@@ -1,40 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.XR;
 
-public class MoveRigidbody : MonoBehaviour
+public class MoveChar : MonoBehaviour
 {
-    public Rigidbody Rb;
-    public Transform cam;
+    //Movimento
+    public CharacterController controller;
     public float moveSpeed = 5f;
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
-    Vector2 moveD = Vector2.zero;
+    private Vector2 moveD = Vector2.zero;
+    //Pulo
+    private Vector2 jumpD = Vector2.zero;
+    public float jumpVelocity = 15;
+    public float gravity = -20f;
+    private bool isJumping = false;
+
+    //camera
+    public Transform cam;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveD = context.ReadValue<Vector2>();
     }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        isJumping = context.ReadValueAsButton();
+        Debug.Log(isJumping);
+    }  
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        playerJump();
     }
-
     private void MovePlayer()
     {
         Vector3 movePos = new Vector3(moveD.x * moveSpeed, 0f, moveD.y * moveSpeed).normalized;
 
-        if (movePos.magnitude >= 0.1f)
+        if (movePos.magnitude >= 0.1f )
         {
             float targetAngle = Mathf.Atan2(moveD.x, moveD.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Rb.velocity = new Vector3(moveD.x * moveSpeed, 0f, moveD.y * moveSpeed);
+            controller.Move(moveDir * moveSpeed * Time.deltaTime);
         }
     }
-
-
+    public void playerJump() 
+    {
+      //jumpD.y = jumpVelocity;
+    }
 }
--- Aprendizado/Assets/Scripts/MoveTransform.cs
+++ Aprendizado/Assets/Scripts/MoveTransform.cs
@@ -4,7 +4,11 @@ using UnityEngine.InputSystem;
 public class MoveTransform : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
     private Vector2 moveD = Vector2.zero;
+
+    public Transform cam;
 
     public void OnMove(InputAction.CallbackContext context) 
     {
@@ -14,13 +18,18 @@ public class MoveTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movePlayer();
+        Vector3 movePos = new Vector3(moveD.x * moveSpeed, 0f, moveD.y * moveSpeed).normalized;
+
+        if (movePos.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(moveD.x, moveD.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
+        }
     }
 
-    public void movePlayer() 
-    {
-        Vector3 movePos = new Vector3(moveD.x * moveSpeed, 0f, moveD.y * moveSpeed);
-        transform.Translate(movePos * moveSpeed * Time.deltaTime, Space.World);
-    }
 
 }
